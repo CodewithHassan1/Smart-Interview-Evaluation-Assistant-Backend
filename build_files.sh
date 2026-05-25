9f4ad64fcbebd77cab73fe806710dbca68c4d6a3
Hassan Haider <hassanhaiderwk@gmail.com>
2026-05-25 15:12:51 +0500

    Use virtualenv in build_files.sh so Vercel builds can install dependencies (avoid PEP 668 errors)

--- build_files.sh
+++ build_files.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
-python3 -m pip install -r requirements.txt
-python3 manage.py collectstatic --noinput --clear
+set -e
+
+# Create and activate a virtualenv so pip installs do not attempt to
+# modify the system Python (fixes PEP 668 "externally-managed-environment" errors).
+python3 -m venv .venv
+source .venv/bin/activate
+pip install --upgrade pip
+pip install -r requirements.txt
+
+# Run collectstatic using the venv's Python
+python manage.py collectstatic --noinput --clear
